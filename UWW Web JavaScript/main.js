ee33818c6a84e6b26e752845790125e1589b2ee5
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2020-03-16 12:34:05 +0100

    checbox test werkt en email verzenden ook. Nu nog bezig om response niet op aparte pagina te krijgen

---
+++ UWW Web JavaScript/main.js
@@ -0,0 +1,20 @@
+
+// test update price on checkbox values
+var total = 0;
+
+var cb1 = document.getElementById("cb1");
+var cb2 = document.getElementById("cb2");
+var cb3 = document.getElementById("cb3");
+
+function clickCB(add, cb) {
+    console.log("CB CLick: " + add + " CB: " + cb)
+
+    if (cb.checked)
+        total += add;
+    else
+        total -= add;
+
+    document.getElementById("total").innerHTML = total;
+}
+
+// send email
